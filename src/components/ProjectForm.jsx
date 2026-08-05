d9fbf88c7c0fd654ed82f47f0b90221e5ad79893
alm <allan@example.com>
2026-08-05 10:12:21 +0300

    created project structure and the component hierachy

---
+++ src/components/ProjectForm.jsx
@@ -0,0 +1,9 @@
+function ProjectForm() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default ProjectForm;
---
+++ src/components/Projectlist.jsx
@@ -0,0 +1,13 @@
+import projectcard from './Projectcard';
+
+function ProjectList() {
+    return (
+        <div>
+            <h2>Projects</h2>
+
+            <ProjectCard/>
+        </div>
+    );
+}
+
+export default ProjectList;
