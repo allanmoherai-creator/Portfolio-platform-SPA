99797b64cf8e1565fbf9722ce4f72d2b81f03a74
alm <allan@example.com>
2026-08-05 13:23:50 +0300

    Created control forms for the project

--- src/components/ProjectForm.jsx
+++ src/components/ProjectForm.jsx
@@ -1,8 +1,28 @@
-function ProjectForm() {
-    return (
-        <div>
+import {useState} from 'react';
 
-        </div>
+function ProjectForm() {
+    const [title, setTitle]=useState("")
+    const [description,setDescription]= useState("")
+    return (
+        <form>
+            <input
+            type= "text"
+            placeholder="Project Title"
+            value={title}
+            onChange={(evt)=> setTitle(evt.target.value)}
+
+            />
+            <textarea
+            type= "text"
+            placeholder= "Project Description"
+            value= {description}
+            onChange={(evt)=> setDescription(evt.target.value)}
+            />
+            <button type="submit">
+                Add Project
+            </button>
+
+        </form>
     );
 }
 
