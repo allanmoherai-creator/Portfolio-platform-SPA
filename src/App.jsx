3ac7ab15d65d6be2a97774c8cfafe1e2b09abe5b
alm <allan@example.com>
2026-08-06 16:12:23 +0300

    corrected some typos

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import {useState} from 'react';
 
 import Header from './components/Header';
 import ProjectForm from './components/ProjectForm';
-import SearchBar from './components/SearchBbar';
+import SearchBar from './components/SearchBar';
 import ProjectList from './components/Projectlist';
 
 
